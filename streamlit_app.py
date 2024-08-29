d7ccd8f7420831605c97faf5a5c67f7a6d00f0d7
ChristopheMontoriol <164173108+ChristopheMontoriol@users.noreply.github.com>
2024-08-29 15:48:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,8 +194,8 @@ elif page == pages[2]:
     matrix_corr.update_layout(title='Matrice de corrélation des salaires',
                           xaxis=dict(title='Variables'),
                           yaxis=dict(title='Variables'),
-                          width=1200,
-                          height=1800)
+                          width=1800,
+                          height=800)
 
 # Affichage du graphique avec Streamlit
     st.plotly_chart(matrix_corr)
