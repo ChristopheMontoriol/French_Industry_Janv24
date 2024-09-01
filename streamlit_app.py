5069ddf38afe3ed9794ec4723c406b0d563f695f
YacineOud <yacine.oudmine@gmail.com>
2024-09-01 22:55:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -293,7 +293,7 @@ elif page == pages[4]:
     st.subheader("Objectif")
     st.write("Prédire le salaire net moyen en fonction des features.")
     
-    if st.expander("Modèles étudiés") :
+    with st.expander("Modèles étudiés") :
         st.write("Liste des modèles")
         st.write("""
                     Afin de déterminer le plus performant possible, nous avons étudié plusieurs modèles de machine learning:
